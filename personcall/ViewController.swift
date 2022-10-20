62e1c42391c7b3dcd7d34380f797087dafe7ee07
fabianbohn <Mystek.fb@googlemail.com>
2022-10-20 11:09:29 +0200

    Initial Commit

---
+++ personcall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  personcall
+//
+//  Created by Fabian Bohn on 20.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
